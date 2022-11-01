1f6ce139cbcd37060a71f33850284f5d4792708a
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-01 09:35:41 -0300

    inicio

---
+++ TF_LOL/src/main.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Marcelo
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
