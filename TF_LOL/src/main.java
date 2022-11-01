44d540716a71225a1dcaa9ea22c65abcc4db56bc
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-01 13:42:23 -0300

    afterignore

--- TF_LOL/src/main.java
+++ TF_LOL/src/main.java
@@ -1,15 +1,8 @@
 
-/**
- *
- * @author Marcelo
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("--------------------------------------------------------");
     }
     
 }
