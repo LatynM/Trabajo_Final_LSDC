eea9d821408cb7ec455b110a4b3a9a95c591dd67
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-02 15:45:41 -0300

    add

---
+++ TF_LOL/TF_LOL/src/tf_lol/TF_LOL.java
@@ -0,0 +1,16 @@
+package tf_lol;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TF_LOL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
