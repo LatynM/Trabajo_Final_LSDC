c1e01046bad1ae848896e147772d7b960ee8f190
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-02 15:35:15 -0300

    srcadd

---
+++ TF_LOL/src/tf_lol/TF_LOL.java
@@ -0,0 +1,13 @@
+package tf_lol;
+
+public class TF_LOL {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
