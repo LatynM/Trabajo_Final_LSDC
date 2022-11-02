9bcf372d445611f3f03bfd6c231f520ea9e7b8b7
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-02 15:50:44 -0300

    ventana

--- TF_LOL/TF_LOL/src/tf_lol/TF_LOL.java
+++ TF_LOL/TF_LOL/src/tf_lol/TF_LOL.java
@@ -1,16 +1,13 @@
 package tf_lol;
 
-/**
- *
- * @author Marcelo
- */
 public class TF_LOL {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ventana v = new Ventana();
+        
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
     }
     
 }
