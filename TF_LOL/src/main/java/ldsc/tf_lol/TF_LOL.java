f029ba2a21ce35bb2c0de2dd6e8bc971ead8bb90
Marcelo Latyn Schiavon <latsc20@gmail.com>
2022-11-05 20:38:34 -0300

    lista

--- TF_LOL/src/main/java/ldsc/tf_lol/TF_LOL.java
+++ TF_LOL/src/main/java/ldsc/tf_lol/TF_LOL.java
@@ -4,6 +4,7 @@ package ldsc.tf_lol;
 public class TF_LOL {
 
     public static void main(String[] args) {
+        
         Ventana window = new Ventana();
         
         window.setVisible(true);
--- TF_LOL/src/main/java/ldsc/tf_lol/Ventana.java
+++ TF_LOL/src/main/java/ldsc/tf_lol/Ventana.java
@@ -12,6 +12,8 @@ public class Ventana extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(800, 600));
@@ -19,15 +21,28 @@ public class Ventana extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Ventana"));
         jPanel1.setPreferredSize(new java.awt.Dimension(1000, 600));
 
+        jList1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jList1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Ashe", "Gwen", "Hecarim", "Jhin", "Leona", "Lux", "Nocturne", "Pantheon", "Ryze", "Twisted Fate", "Zac" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jList1.setFixedCellHeight(100);
+        jScrollPane1.setViewportView(jList1);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 958, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(718, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 496, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 496, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -48,6 +63,8 @@ public class Ventana extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList<String> jList1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
